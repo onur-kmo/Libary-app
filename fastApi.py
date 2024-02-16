852cbbb0e6647b169d87980da63b1d92d6f0234e
onur karamahmutoğlu <150650164+onur-kmo@users.noreply.github.com>
2024-02-16 21:44:04 +0300

    Update fastApi.py

--- fastApi.py
+++ fastApi.py
@@ -22,5 +22,5 @@ def fastapi_endpoint():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(fastapi_app, host="127.0.0.1", port=8000)
+    uvicorn.run(fastapi_app, host="0.0.0.0", port=8000)
     
